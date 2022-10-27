6a578675c325dffa078e2b20c1da34b418f19107
Steven Vergenz <vergenzs@gmail.com>
2022-10-27 09:54:27 -0700

    Fix export

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,1 @@
-declare module '@darkeyedevelopers/natural-cron.js'
-{
-    function getCronString(inputString: string, syntaxString?: string): string;
-}
+export default function getCronString(inputString: string, syntaxString?: string): string;
